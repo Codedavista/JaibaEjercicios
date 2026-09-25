ff9b9139582b0a16b406111bd8a2e2505c589149
Luis Uriel Guerra Patiño <luisguerra@comunidad.unam.mx>
2026-09-25 20:34:50 +0000

    Ejercicio 3 completo

---
+++ Exercises/EspirituSanto.java
@@ -0,0 +1,15 @@
+package Exercises;
+
+public class EspirituSanto extends Hijo{
+    String apellidoReal;
+
+    public EspirituSanto(String nombre, String apellido, String apellidoReal){
+        super(nombre, apellido);
+    this.apellidoReal = apellidoReal;
+    }
+
+    public Boolean pruebaPaternidad(){
+        return apellido.equals(apellidoReal);
+    }
+
+}
